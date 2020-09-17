f808892096503cf480d81da5fd70d646ccbed2ab
Matheus Boy <matheus.boy@i-value.com.br>
2020-09-17 12:47:23 -0300

    100% code coverage in ackermmann

--- core/tests/ackermann_test.py
+++ core/tests/ackermann_test.py
@@ -8,3 +8,9 @@ class AckermannTest(TestCase):
         for i in range(0, 4):
             for j in range(0, 10):
                 assert ackermann_naive(i, j) == ackermann(i, j)
+
+    def test_ackermann_4_0(self):
+        assert ackermann(4, 0) == 13
+
+    def test_ackermann_4_1(self):
+        assert ackermann(4, 1) == 65533
